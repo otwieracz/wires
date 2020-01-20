202bf8068a7c8390cb823fc07616ac16548852d6
Slawomir Gonet <slawek@otwiera.cz>
2020-01-20 12:39:30 +0100

    Setup system components, aero for configuration and datomic peer (WIP)

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,9 +1,11 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [set-refresh-dirs refresh]]
             [system.repl :refer [system set-init! start stop reset]]
-            [wires.systems :refer [dev-system]]
             [shadow.cljs.devtools.api :refer [repl]]
-            [datomic.api :as datomic]))
+            [datomic.api :as datomic]
+            [datomic.pull :refer [pull]]
+            [wires.systems :refer [dev-system]]
+            [wires.datomic :as wd]))
 
 ;; Ensure we only refresh the source we care about. This is important
 ;; because `resources` is on our classpath and we don't want to
@@ -11,3 +13,14 @@
 (set-refresh-dirs "src/dev" "src/main")
 
 (set-init! #'dev-system)
+
+(defn conn [] (:conn (:datomic-db system)))
+(defn db [] (datomic/db (conn)))
+
+(defn reset-and-load
+  []
+  (do
+    (reset)
+    (wd/load-schema (conn))
+    (wd/load-initial-data (conn))
+    true))
--- src/main/wires/datomic.clj
+++ src/main/wires/datomic.clj
@@ -1,11 +1,26 @@
 (ns wires.datomic
-  (:require [datomic.api :as d]
-            ))
+  (:require [datomic.api :as datomic]))
+
+(def terminal-schema
+  [{:db/ident       :terminal/id
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
+    :db/doc         "Terminal ID"}
+   {:db/ident       :terminal/connector
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Terminal connector"}
+   {:db/ident       :terminal/pin
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Terminal pin"}])
 
 (def wire-schema
   [{:db/ident       :wire/id
     :db/valueType   :db.type/long
     :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
     :db/doc         "Wire ID"}
    {:db/ident       :wire/label
     :db/valueType   :db.type/string
@@ -20,4 +35,39 @@
     :db/cardinality :db.cardinality/many
     :db/doc         "Wire Terminals"}])
 
+(def terminals-data
+  [{:terminal/id        0
+    :terminal/connector "JST.0"
+    :terminal/pin       1}
+   {:terminal/id        1
+    :terminal/connector "JST.0"
+    :terminal/pin       2}
+   {:terminal/id        2
+    :terminal/connector "SCREWED.0"
+    :terminal/pin       0}
+   {:terminal/id        3
+    :terminal/connector "SCREWED.0"
+    :terminal/pin       1}])
+
+(def wires-data
+  [{:wire/id        0
+    :wire/label     "Vcc"
+    :wire/color     "red"
+    :wire/terminals [[:terminal/id 0]
+                     [:terminal/id 2]]}
+   {:wire/id        1
+    :wire/label     "GND"
+    :wire/color     "brown"
+    :wire/terminals [[:terminal/id 1]
+                     [:terminal/id 3]]}])
+
+(defn load-schema
+  [conn]
+  (map (fn [schema] @(datomic/transact conn schema))
+       [terminal-schema wire-schema]))
+
+(defn load-initial-data
+  [conn]
+  (map (fn [data] @(datomic/transact conn data))
+       [terminals-data wires-data]))
 
